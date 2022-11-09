b6413dc78db194c6ba287803146a2484e6197187
True-Mousse <101472540+True-Mousse@users.noreply.github.com>
2022-11-09 12:09:49 -0800

    Delete Create and Set Screen Saver Timeout.ps1

--- Create and Set Screen Saver Timeout.ps1
+++
@@ -1,7 +0,0 @@
-# Creates and links a GPO called ScreenSaverTimeout to the Marketing OU
-
-New-GPO -Name "ScreenSaverTimeout" -Comment "Sets timeout to 120 secs"
-
-Set-GPRegistryValue -Name "ScreenSaverTimeOut" -Key "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" -ValueName ScreenSaveTimeOut -Type String -Value 120
-
-New-GPLink -Name "ScreenSaverTimeout" -Target "ou=Marketing,dc=it305,dc=cmit305,dc=com"
