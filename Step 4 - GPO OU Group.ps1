0374295a11e01b9fe5d099c4a5dd0f24747896da
True-Mousse <101472540+True-Mousse@users.noreply.github.com>
2022-11-09 19:06:05 -0800

    Added Password Policy

--- Step 4 - GPO OU Group.ps1
+++ Step 4 - GPO OU Group.ps1
@@ -1,6 +1,4 @@
-﻿## Adds Users to AD from CSV file. 
-##
-
+## Adds Users to AD from CSV file. 
 $CSV_File = C:\Users\Administrators\Desktop\New_Users.csv # Location of CSV File
 $Users = Import-csv $CSV_File # Imports data from CSV 
 
@@ -14,11 +12,9 @@ foreach($User in $Users) # Iterates through rows imported from CSV
 }
 
 ## Change logon hours from 8AM to 8PM
-##
-
 $File = Get-Content C:\Users\Administrator\Desktop\Users.txt # Location of user file; file must contain usernames from AD 
 
-[byte[]]$hours = @(15,0,255,15,0,255,15,0,255,15,0,255,15,0,255,15,0,255,15,0,255) # Array w/ logon hours set to 8AM - 8PM (binary format)
+[byte[]]$hours = @(15,0,255,15,0,255,15,0,255,15,0,255,15,0,255,15,0,255,15,0,255) # Array w/ logon hours set to 8AM - 8PM (binary format)S
 
 foreach($I in $File) # Iterates through array to set logon hours for user
 {
@@ -26,9 +22,7 @@ foreach($I in $File) # Iterates through array to set logon hours for user
 }
 
 ## Creates a dynamic VHD of 40GB in size
-
 $VHD_Path = "C:\Users\Administrators\Desktop\VHD_File.vhd" # Location to save VHD File
-
 NEW-VHD -Dynamic $VHD_Path -SizeBytes 40GB 
 
 ## Creates Security Group "Marketing" in Active Directory (Shadow Group)
@@ -38,10 +32,18 @@ New-ADGroup -Name "Marketing" -groupscope Global
 NEW-ADOrganizationalUnit “Marketing”
 
 ## Create and link Screen Saver GPO to the Marketing OU
-
 New-GPO -Name "ScreenSaverTimeout" -Comment "Sets timeout to 120 secs" # Creation of GPO 
  
 Set-GPRegistryValue -Name "ScreenSaverTimeOut" -Key "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" -ValueName ScreenSaveTimeOut -Type String -Value 120 # Changes GP Registry 
 
-New-GPLink -Name "ScreenSaverTimeout" -Target "ou=Marketing,dc=it305,dc=cmit305,dc=com" # Links GPO to Marketing OU
+New-GPLink -Name "ScreenSaverTimeOut" -Target "ou=Marketing,dc=it305,dc=cmit305,dc=com" # Links GPO to Marketing OU
 
+## Create and set a Fine-Grained Password Policy Object (PSO) for the Marketing Group
+## Password Policy Settings: 
+##       Minimum Password History: 12
+##       Minimum Password Length: 20
+##       Complexity: True
+New-ADFineGrainedPasswordPolicy -Name "MarketingPSO" -Precedence 10 -Description "The Marketing Group Password Policy" -DisplayName "Marketing Group PSO" -ComplexityEnabled $true -MinPasswordLength 20 -PasswordHistoryCount 12
+
+## Links Fine-Grained Password Object to the Marketing Security Group (Shadow Group)
+Add-ADFineGrainedPasswordPolicySubject -Identity "MarketingPSO" -Subjects "Marketing"
